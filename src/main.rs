6bf1793f4ad38dd1bb6c992116c25cd95a81e86e
Tomas Siegl <tomas.siegl@gmail.com>
2020-04-18 23:35:21 +0200

    Try to decode body

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 //extern crate async_listen;
 extern crate async_std;
-extern crate libfrps_rs;
 extern crate http_types;
+extern crate libfrps_rs;
 
 //use async_listen::{backpressure, error_hint, ByteStream, ListenExt};
 use async_std::net::{TcpListener, TcpStream, ToSocketAddrs};
 use async_std::prelude::*;
 use async_std::task;
-use http_types::{Response, StatusCode};
+use async_std::io::{BufRead, Read};
+use http_types::headers::{HeaderName, HeaderValue};
+use http_types::{Body, Headers, Response, StatusCode};
+use libfrps_rs::{Serializer, Tokenizer, Value, ValueTreeBuilder};
 use std::io;
 use std::time::Duration;
-use libfrps_rs::{Serializer, Tokenizer, Value, ValueTreeBuilder};
+use std::str::FromStr;
 
 #[async_std::main]
 async fn main() -> http_types::Result<()> {
     // Open up a TCP connection and create a URL.
-    let listener = TcpListener::bind(("127.0.0.1", 8080)).await?;
+    let listener = TcpListener::bind(("127.0.0.1", 30001)).await?;
     let addr = format!("http://{}", listener.local_addr()?);
     println!("listening on {}", addr);
 
@@ -41,19 +44,71 @@ async fn main() -> http_types::Result<()> {
 async fn accept(addr: String, stream: TcpStream) -> http_types::Result<()> {
     println!("starting new connection from {}", stream.peer_addr()?);
     async_h1::accept(&addr, stream.clone(), |req| async move {
-        if let Some(val) = req.header("User-agent") {
-            println!("User-agent: {}", val)
+
+        let content_length = HeaderName::from_str("content-length").unwrap();
+        let content_type = HeaderName::from_str("content-type").unwrap();
+        let accept = HeaderName::from_str("accept").unwrap();
+        let user_agent = HeaderName::from_str("user-agent").unwrap();
+        
+        if let Some(val) = req.header(&user_agent) {
+            println!("User-agent: {}", val[0])
         }
 
-        if Some(val) = req.header("Accept") {
-            println!("Accept: {}", val)
+        // Get type of protocol
+        let mut is_frps = false;
+        if let Some(val) = req.header(&content_type) {
+            let val = &val[0];
+            println!("Content-type: {}", val);
+
+            match val.as_str() {
+                "application/x-frpc" => is_frps = false,
+                "application/x-frps" => is_frps = true,
+                _ => {
+                    let mut res = Response::new(StatusCode::BadRequest);
+                    res.insert_header("Content-Type", "text/plain")?;
+                    res.set_body("Invalid content-type expected: either application/x-frpc or application/x-frps");
+                    return Ok(res)
+                } 
+            }
+        }
+
+        if let Some(val) = req.header(&content_length) {
+            println!("Content-lenght: {}", val[0])
+        }
+
+        // Get type of response supported by client. We prefer frps
+        let mut response_as_frps = is_frps;
+        if let Some(val) = req.header(&accept) {
+            for v in val {
+                println!("Accept: {}", v);
+                if v.as_str().contains("application/x-frps") {
+                    response_as_frps = true;
+                    break;
+                }
+            }
+        }
+
+        let body: Body = req.into();
+        let body: String = body.into_string().await?;
+
+        // parse
+        let mut tokenizer = if is_frps {
+            Tokenizer::new_frps()
+        } else {
+            Tokenizer::new_frpc()
         };
 
-        if Some(val) = req.header("Content-Type") {
-            println!("Content-Type: {}", val)
-        };
-            
-        let ct = req.header(name);
+        let mut values = ValueTreeBuilder::new();
+
+        match tokenizer.parse(body.as_bytes(), &mut values) {
+            OK(finished, len) => {},
+            Err(_) => {
+                let mut res = Response::new(StatusCode::BadRequest);
+                res.insert_header("Content-Type", "text/plain")?;
+                res.set_body("Invalid body, can't deserialize");
+                return Ok(res)
+            }
+        }
 
         let mut res = Response::new(StatusCode::Ok);
         res.insert_header("Content-Type", "text/plain")?;
